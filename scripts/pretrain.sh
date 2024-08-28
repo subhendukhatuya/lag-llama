454b2eff26b09fd606df675595ebde4be3af346c
Barun Parua <76466796+Baron105@users.noreply.github.com>
2024-08-28 19:41:52 +0530

    Update pretrain.sh

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -56,7 +56,7 @@ do
     -e $EXP_NAME -d "datasets" --seed $SEED \
     -r "experiments/results" \
     --batch_size 256 -m 10 -n 128 \
-    --wandb_entity barunparua-iit-kharagpur --wandb_project lag-llama-test \
+    --wandb_entity barunparua-iit-kharagpur --wandb_project lag-llama-test-2 \
     --all_datasets "australian_electricity_demand" "electricity_hourly" "london_smart_meters_without_missing" "solar_10_minutes" "wind_farms_without_missing" "pedestrian_counts" "uber_tlc_hourly" "traffic" "kdd_cup_2018_without_missing" "saugeenday" "sunspot_without_missing" "exchange_rate" "cpu_limit_minute" "cpu_usage_minute" "function_delay_minute" "instances_minute" "memory_limit_minute" "memory_usage_minute" "platform_delay_minute" "requests_minute" "ett_h1" "ett_h2" "ett_m1" "ett_m2" "beijing_pm25" "AirQualityUCI" "beijing_multisite" "weather" \
     --test_datasets "weather" "pedestrian_counts" "exchange_rate" "ett_m2" "platform_delay_minute" "requests_minute" "beijing_pm25" \
     --num_workers 2 --args_from_dict_path $CONFIGPATH --search_batch_size \
